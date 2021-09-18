cd2dc336a129bb16fa991f1f545248ef6ac360ec
Felipe Pacheco Schürmann <felipe.xirma@gmail.com>
2021-09-18 12:45:21 +0100

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeContainerComponent } from './containers/home-container/home-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeContainerComponent
   ],
   imports: [
     BrowserModule,
