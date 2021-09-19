04acb0040fddd9687083da3717cc24b6e30bd423
Felipe Pacheco Schürmann <felipe.xirma@gmail.com>
2021-09-19 19:35:48 +0100

    tweets timestamp

--- src/app/components/tweets-list/tweets-list.component.ts
+++ src/app/components/tweets-list/tweets-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 import { MainService } from 'src/app/main.service';
 import { Tweet } from 'src/app/models/tweet';
 
@@ -8,7 +9,26 @@ import { Tweet } from 'src/app/models/tweet';
   styleUrls: ['./tweets-list.component.scss']
 })
 export class TweetsListComponent implements OnInit {
+
   tweets: Tweet[];
+  tweetDate = timer(1000, 1000);
+  dateObservable = this.tweetDate.subscribe((val) => {
+    this.tweets.forEach((item, index) => {
+      const dateNow = new Date();
+      const timeNow = dateNow.getTime();
+      const tweetDate = new Date(item.date);
+      const tweetTime = tweetDate.getTime();
+      const dif = timeNow - tweetTime;
+      const seconds = Math.round(dif / 1000);
+
+      if (seconds <= 60) {
+        item.dateToShow = `${seconds.toString()} seconds ago.`;
+        return;
+      }
+      item.dateToShow = `${tweetDate.toLocaleDateString()} at ${tweetDate.toLocaleTimeString()} `;
+    });
+  });
+
 
   constructor(
     private service: MainService
